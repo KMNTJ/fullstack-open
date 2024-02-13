6d80e4f5607556b2c526410012358cfac6511b52
Tuomas Nurminen <tuomas.nurminen.tietoevry@ahti.cloud>
2024-02-13 17:37:38 +0200

    part1 a done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,55 @@
-const Hello = (props) => {
-  return <p>Hello {props.foo}</p>;
+const Header = (props) => {
+  return <p>{props.name}</p>;
+};
+
+const Content = (props) => {
+  let content = [];
+  props.parts.forEach((part) => {
+    content.push(<Part part={part}></Part>);
+  });
+  return <>{content}</>;
+};
+
+const Part = (props) => {
+  let part = props.part;
+  return (
+    <div>
+      <p>{part.partDescription}</p>
+      <p>{part.excercises}</p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  let total = 0;
+  props.parts.forEach((part) => {
+    total += part.excercises;
+  });
+  return <p>{`Number of exercises ${total}`}</p>;
 };
 
 const App = () => {
-  const bar = "qwe";
-  const flo = ['dfg', 'hrty'];
+  const course = "Half Stack application development";
+  const parts = [
+    {
+      partDescription: "Fundamentals of React",
+      excercises: 10,
+    },
+    {
+      partDescription: "Using props to pass data",
+      excercises: 7,
+    },
+    {
+      partDescription: "State of a component",
+      excercises: 14,
+    },
+  ];
 
   return (
     <div>
-      <Hello foo="asd" />
-      <Hello foo={bar} />
-      <p>{flo}</p>
+      <Header name={course}></Header>
+      <Content parts={parts}></Content>
+      <Total parts={parts}></Total>
     </div>
   );
 };
