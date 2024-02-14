89f8b980e712a026eb61ba6d5c85c5ac36ebbf23
Tuomas Nurminen <tuomas.nurminen.tietoevry@ahti.cloud>
2024-02-14 15:23:19 +0200

    1.6 unicafe step1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const Tally = ({ text, count }) => {
+  return (
+    <p>
+      {text} {count}
+    </p>
+  );
+};
+
 const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
+  };
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+  const handleBad = () => {
+    setBad(bad + 1);
+  };
 
   return (
     <div>
-      <p>Hello</p>
+      <h1>Give feedback</h1>
+      <Button handleClick={handleGood} text="good"></Button>
+      <Button handleClick={handleNeutral} text="neutral"></Button>
+      <Button handleClick={handleBad} text="bad"></Button>
+      <h2>Statistics</h2>
+      <Tally text="good" count={good}></Tally>
+      <Tally text="neutral" count={neutral}></Tally>
+      <Tally text="bad" count={bad}></Tally>
     </div>
   );
 };
