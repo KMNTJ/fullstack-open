4c284da094db559fc885381ddf90598d00853871
Tuomas Nurminen <tuomas.nurminen.tietoevry@ahti.cloud>
2024-02-15 19:16:17 +0200

    2.1 kurssitiedot step6 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,86 @@
 const Header = (props) => {
   let courseName = props.course.name;
-  return <p>{courseName}</p>;
+  return <h1>{courseName}</h1>;
 };
 
-const Content = (props) => {
-  let content = [];
-  props.course.parts.forEach((part) => {
-    content.push(<Part part={part}></Part>);
-  });
-  return <>{content}</>;
-};
+// const Content = (props) => {
+//   let content = [];
+//   props.course.parts.forEach((part) => {
+//     content.push(<Part part={part}></Part>);
+//   });
+//   return <>{content}</>;
+// };
 
-const Part = (props) => {
-  let part = props.part;
+// const Part = (props) => {
+//   let part = props.part;
+//   return (
+//     <div>
+//       <p>{part.name}</p>
+//       <p>{part.excercises}</p>
+//     </div>
+//   );
+// };
+
+// const Total = (props) => {
+//   let total = 0;
+//   props.course.parts.forEach((part) => {
+//     total += part.excercises;
+//   });
+//   return <p>{`Number of exercises ${total}`}</p>;
+// };
+
+const Part = ({ part }) => {
   return (
-    <div>
-      <p>{part.name}</p>
-      <p>{part.excercises}</p>
-    </div>
+    <li>
+      {part.name} {part.excercises}
+    </li>
   );
 };
 
-const Total = (props) => {
-  let total = 0;
-  props.course.parts.forEach((part) => {
-    total += part.excercises;
-  });
-  return <p>{`Number of exercises ${total}`}</p>;
+const Content = ({ course }) => {
+  return course.parts.map((part) => <Part key={part.id} part={part}></Part>);
 };
 
-const App = () => {
-  const course = {
-
-
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        excercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        excercises: 7,
-      },
-      {
-        name: "State of a component",
-        excercises: 14,
-      },
-    ]
-  }
-
+const Course = ({ course }) => {
   return (
     <div>
       <Header course={course}></Header>
       <Content course={course}></Content>
-      <Total course={course}></Total>
     </div>
   );
 };
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        id: 1,
+        name: "Fundamentals of React",
+        excercises: 10,
+      },
+      {
+        id: 2,
+        name: "Using props to pass data",
+        excercises: 7,
+      },
+      {
+        id: 3,
+        name: "State of a component",
+        excercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Course course={course}></Course>
+    </div>
+  );
+};
+
+// <Header course={course}></Header>
+// <Content course={course}></Content>
+// <Total course={course}></Total>
 
 export default App;
