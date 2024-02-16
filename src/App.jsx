df0e6b8a9e75c6fedadb6dae8695c0aa2383de32
Tuomas Nurminen <tuomas.nurminen.tietoevry@ahti.cloud>
2024-02-16 12:42:36 +0200

    2.8 puhelinluettelo step3 done

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ const Numbers = ({ persons }) => {
   return (
     <div>
       {persons.map((x) => (
-        <div key={x.name}>{x.name}</div>
+        <div key={x.name}>{x.name} {x.number}</div>
       ))}
     </div>
   );
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: '0401234567' }]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -20,13 +21,18 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       return;
     }
-    const newPerson = { name: newName };
+    const newPerson = { name: newName, number: newNumber };
     setPersons(persons.concat(newPerson));
     setNewName('');
+    setNewNumber('');
   };
 
   const nameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const numberChange = (event) => {
+    setNewNumber(event.target.value);
   };
   
   return (
@@ -35,6 +41,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={nameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={numberChange} />
         </div>
         <div>
           <button type="submit">add</button>
