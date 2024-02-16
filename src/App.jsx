f308210e7b19b5aad6cfca3224f5ed62d17e1d16
Tuomas Nurminen <tuomas.nurminen.tietoevry@ahti.cloud>
2024-02-16 13:17:40 +0200

    2.9 puhelinluettelo step4 done

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,23 @@ const Numbers = ({ persons }) => {
   );
 };
 
+const Filter = ({handleChange}) => {
+  return (
+    <div>filter shown with<input onChange={handleChange}></input></div>
+  )
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: '0401234567' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [shownPersons, setShownPersons] = useState(persons);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -34,10 +47,18 @@ const App = () => {
   const numberChange = (event) => {
     setNewNumber(event.target.value);
   };
+
+  const filterValueChange = (event) => {
+    const currentFilter = event.target.value;
+    const filteredPersons = persons.filter((x) => x.name.toLowerCase().indexOf(currentFilter.toLowerCase()) !== -1)
+    setShownPersons(filteredPersons);
+  };
   
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter handleChange={filterValueChange}></Filter>
+      <h3>Add a new</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={nameChange} />
@@ -49,8 +70,8 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <Numbers persons={persons}></Numbers>
+      <h3>Numbers</h3>
+      <Numbers persons={shownPersons}></Numbers>
     </div>
   );
 };
