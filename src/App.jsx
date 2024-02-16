34c72673f5a3fc3034e34c099d33f9f9b64a7a3e
Tuomas Nurminen <tuomas.nurminen.tietoevry@ahti.cloud>
2024-02-16 11:53:46 +0200

    2.6 puhelinluettelo step1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
+import { useState } from "react";
+
+const Numbers = ({ persons }) => {
+  return (
+    <div>
+      {persons.map((x) => (
+        <div key={x.name}>{x.name}</div>
+      ))}
+    </div>
+  );
+};
+
 const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName };
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+  };
+
+  const nameChange = (event) => {
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
-      <p>Hello</p>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={nameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Numbers persons={persons}></Numbers>
     </div>
   );
 };
