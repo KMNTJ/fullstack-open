fd49be77c1018206f0917b09a83027a3512ef7ff
Tuomas Nurminen <tuomas.nurminen.tietoevry@ahti.cloud>
2024-03-04 15:37:41 +0200

    3.9 puhelinluettelo backend step 9 10 11 done

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mkcert from'vite-plugin-mkcert'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    https: true
-  }, // Not needed for Vite 5+ (simply omit this option)
   plugins: [react(), mkcert()],
-})
+  server: {
+    https: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+    },
+  },
+});
